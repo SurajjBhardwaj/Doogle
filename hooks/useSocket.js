2076d587446d6fe2690417588b4994e67b06dd9d
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-02-24 12:25:28 +0530

    Added a socket provider to use socket.io in our project

---
+++ hooks/useSocket.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+
+const useSocket = () => {
+  const socketCreated = useRef(false);
+  useEffect(() => {
+    if (!socketCreated.current) {
+      const socketInitializer = async () => {
+        await fetch("/api/socket");
+      };
+      try {
+        socketInitializer();
+        socketCreated.current = true;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, []);
+};
+
+export default useSocket;
