4df0410111657cb84d87725d4e7f6090cacbb2ae
JyotiKM29 <jyotikumari4442@gmail.com>
2024-02-25 02:11:47 +0530

    Home page responsive

--- sections/profile/Home.js
+++ sections/profile/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     <section className="w-screen h-screen bg-purple-100 ">
       <div className="mx-auto max-w-screen-2xl px-8 md:px-16 py-2">
         <header className="h-14 flex w-full justify-between py-4">
-          <img src="/Doogle.svg" alt="logo" className=" self-start " />
+          <Image height={100} width={200} src="/Doogle.svg" alt="logo" className=" self-start h-12 w-[6rem] lg:h-16 lg:w-32" />
 
           <HoverCard>
             <HoverCardTrigger asChild>
@@ -42,33 +42,33 @@ const Home = () => {
           {/* <CircleUserRound  color="#6C2DBE" className="h-8 w-8" /> */}
         </header>
 
-        <div className="my-40 flex items-center justify-center gap-16">
+        <div className="my-20 lg:my-40 flex flex-col lg:flex-row items-center justify-center gap-16">
           <div class=" flex flex-col  justify-center">
-            <span class=" text-4xl font-extrabold tracking-tight text-gray-800 sm:text-6xl">
+            <span class="text-center text-3xl text-nowrap lg:text-left lg:text-6xl font-extrabold tracking-tight text-gray-900 ">
               <h1>Connect with Developer</h1>
-              <h1>Anywhere, Anytime</h1>
+              <h1 >Anywhere, Anytime</h1>
             </span>
-            <p className="mt-6 px-4 text-left text-lg  text-gray-600">
+            <p className=" text-center lg:text-left  mt-6 px-4 text-sm lg:text-xl  text-gray-600">
               Elevate your coding journey with  developers .
                
             </p>
-            <p className="px-4 text-left text-lg  text-gray-600">
+            <p className=" text-center lg:text-left  px-4 text-sm lg:text-xl  text-gray-600">
             Let&apos;s connect and grow
               together!
             </p>
-            <div class="mt-10 flex items-center gap-x-4">
+            <div class="mt-10 flex flex-col gap-y-4 lg:flex-row items-center lg:gap-x-4">
            
   
 
-              <Button className=" p-6 px-16 rounded-full space-x-1">
+              <Button className="p-5 lg:p-6 px-7 lg:px-16 rounded-full space-x-1 w-4/5">
              
-                <p className="text-xl font-light ">
+                <p className="text-base lg:text-xl font-light ">
                 Talk Instantly 
                 </p>
                 <ArrowUpRight strokeWidth={1.5} />
               </Button>
-              <Button className="space-x-3 p-6 px-24 rounded-full " variant="secondary">
-              <p className="text-xl font-light ">
+              <Button className="p-5 lg:p-6 px-7 lg:px-16  space-x-3  rounded-full  w-4/5 bg-white" variant="secondary">
+              <p className="text-base lg:text-xl font-light ">
               Match 
               </p>
                 <Code strokeWidth={1.5} />
@@ -77,7 +77,7 @@ const Home = () => {
           </div>
 
           <div className="">
-            <div className="absolute  w-[600px] h-[500px]  opacity-30 blur-[100px] bg-purple-700 "></div>
+            <div className="absolute top-[10%] w-[90%] h-[80%] lg:w-[600px] lg:h-[500px]  opacity-30 blur-[100px] bg-purple-600 lg:bg-purple-700 "></div>
             <Image
               src="/Home.png"
               height={400}
