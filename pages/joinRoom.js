328535ae5697f0a72a7f65eee357e9acd2c8b9e0
Suraj Pandey <111424454+SurajjBhardwaj@users.noreply.github.com>
2024-02-24 23:49:02 +0530

    Updated the route to join the room

--- pages/joinRoom.js
+++ pages/joinRoom.js
@@ -1,18 +1,25 @@
 // pages/join-room.js
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 // import styles from "../styles/Home.module.css";
 
-export default function JoinRoom({ handleCredChange, handleLogin }) {
+export default function JoinRoom({ handleCredChange }) {
   const [roomName, setRoomName] = useState("");
   const [userName, setUserName] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     handleCredChange(userName, roomName);
   }, [roomName, userName, handleCredChange]);
 
+  const handleLogin = (e) => {
+    e.preventDefault();
+    router.push(`/newRoom/${roomName}`);
+  };
+
   return (
-    <div >
+    <div>
       <Head>
         <title>
           Join Room - Native WebRTC API with NextJS and Pusher as the Signalling
@@ -22,23 +29,19 @@ export default function JoinRoom({ handleCredChange, handleLogin }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <form  onSubmit={handleLogin}>
+      <form onSubmit={handleLogin}>
         <h1>Join a Room</h1>
         <input
           onChange={(e) => setUserName(e.target.value)}
           value={userName}
-          
           placeholder="Enter Username"
         />
         <input
           onChange={(e) => setRoomName(e.target.value)}
           value={roomName}
-          
           placeholder="Enter Room Name"
         />
-        <button type="submit" >
-          Join Room
-        </button>
+        <button type="submit">Join Room</button>
       </form>
     </div>
   );
